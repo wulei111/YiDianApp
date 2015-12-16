f348eac00576d1469b8d4b4f44e4dae88d1766c8
wulei111 <1395995359@qq.com>
2015-12-16 16:54:50 +0800

    gai

---
+++ YiDianApp/app/src/main/java/com/best/fragment/IndexFragment.java
@@ -0,0 +1,21 @@
+package com.best.fragment;
+
+import android.app.Activity;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.best.yidianapp.R;
+
+
+public class IndexFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
